f377268d5297ec8cfefd43635114a5e40bcd433a
Kaustubh <kkaustubh@us.ibm.com>
2019-10-29 10:36:02 -0700

    Added base redux store to the app. Initial test reducer.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  const App = require('./App').default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  );
+};
+
+
+if (process.env.NODE_ENV !== 'production') {
+  if (module.hot) {
+    module.hot.accept('./App', () => {
+      setTimeout(render);
+    })
+  }
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/configureStore.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import rootReducer from '../reducers/rootReducer';
+
+export default function configureStore(preloadedState) {
+  const middlewares = [thunk];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const storeEnhancer = [middlewareEnhancer];
+
+  const composedEnhancer = composeWithDevTools(...storeEnhancer);
+
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    composedEnhancer
+  );
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('../reducers/rootReducer', () => {
+        const newRootReducer = require('../reducers/rootReducer').default;
+        store.replaceReducer(newRootReducer);
+      })
+    }
+  }
+
+  return store;
+}
